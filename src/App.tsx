806a3682bf467bdf127b940c995d2131374e0a16
MuhammadFahadAkbar <fahadakbar200@gmail.com>
2023-06-18 13:38:02 +0500

    Complete JSON Server Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import axios from "axios";
+import { useEffect } from "react";
+
 const App = () => {
-  return <h1>React TSX Starter Template</h1>;
+  useEffect(() => {
+    // fetch("http://localhost:4000/edges")
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     console.log(data);
+    //   });
+    axios("http://localhost:4000/edges").then(({ data }) => {
+      console.log("Products:", data);
+    });
+  }, []);
+  return <h1>Pagination</h1>;
 };
 
 export default App;
