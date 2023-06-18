9396ca3cd0de051e138bfee2f8cd917095d9342b
MuhammadFahadAkbar <fahadakbar200@gmail.com>
2023-06-18 15:17:10 +0500

    Added Pagination

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,76 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
+interface Product {
+  node: {
+    id: string;
+    title: string;
+    variants: {
+      edges: {
+        node: {
+          price: {
+            amount: string;
+          };
+        };
+      }[];
+    };
+    images: {
+      edges: {
+        node: {
+          url: string;
+        };
+      }[];
+    };
+  };
+}
 
 const App = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+  const totalpages = products.length / 5;
+
+  const [pageNumber, setPageNumber] = useState(1);
+
+  const handleNextClick = () => {
+    setPageNumber((prevPage) => {
+      return prevPage < totalpages ? prevPage + 1 : prevPage;
+    });
+  };
+
+  const handlePreviousClick = () => {
+    setPageNumber((prevPage) => {
+      return prevPage > 1 ? prevPage - 1 : prevPage;
+    });
+  };
+
   useEffect(() => {
-    // fetch("http://localhost:4000/edges")
-    //   .then((response) => response.json())
-    //   .then((data) => {
-    //     console.log(data);
-    //   });
     axios("http://localhost:4000/edges").then(({ data }) => {
-      console.log("Products:", data);
+      setProducts(data);
     });
   }, []);
-  return <h1>Pagination</h1>;
+
+  const currentProducts = products.slice((pageNumber - 1) * 5, pageNumber * 5);
+
+  return (
+    <>
+      <h1>Pagination</h1>
+      <div>
+        <ul>
+          {currentProducts.map((product) => (
+            <li>{product.node.title}</li>
+          ))}
+        </ul>
+        <div>
+          {pageNumber > 1 && (
+            <button onClick={handlePreviousClick}>Previous</button>
+          )}
+          <p>{pageNumber}</p>
+          {pageNumber !== totalpages && (
+            <button onClick={handleNextClick}>Next</button>
+          )}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default App;
